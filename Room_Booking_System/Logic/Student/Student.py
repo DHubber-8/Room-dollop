f252b4591fb71f8d3349000456466df222fd5e4a
DHubber-8 <digantdash2002@gmail.com>
2026-05-12 00:15:52 +0800

    Invalid user/pass notif and colour coding of status in reservations

--- Room_Booking_System/Logic/Student/Student.py
+++ Room_Booking_System/Logic/Student/Student.py
@@ -3,7 +3,7 @@ class Student(object):
         pass
     def check_student_login(self,username, password):
     # This function only cares about student verification
-        if username == 'student' and password == 'password123':
+        if username == 'student@uowmail.edu.au' and password == 'password123':
             return True
         return False
 
